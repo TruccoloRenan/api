c5ce1b51498ac6febc3691e5eedb18004caf621d
rennatruccolo <renan4040@gmail.com>
2024-06-13 22:46:58 -0300

    logs ok

--- src/tasks/task.service.ts
+++ src/tasks/task.service.ts
@@ -1,33 +1,57 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { Prisma, Task } from "@prisma/client";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model, Types } from "mongoose";
+import { LogsService } from "src/logs/logs.service";
+import { Log, LogDocument } from '../logs/schemas/log.schema';
 
 @Injectable()
 export class TaskService {
-    constructor(private prisma: PrismaService) {}
+    constructor(
+        @InjectModel(Log.name) private taskModel: Model<LogDocument>,
+        private logService: LogsService,
+        private prisma: PrismaService
+    ) { }
 
     async createTask(data: Prisma.TaskCreateInput): Promise<Task> {
-        return this.prisma.task.create({ data });
+
+        const createdTask = this.prisma.task.create({ data });
+
+        const log = new this.taskModel(data);
+        await log.save();
+        await this.logService.createLog("Task Created " + createdTask);
+
+        return createdTask;
     }
 
-    async findAll(): Promise<Task[]>{
+    async findAll(): Promise<Task[]> {
         return this.prisma.task.findMany()
     }
 
-    async findOne(id: number): Promise<Task>{
-        return this.prisma.task.findUnique({ where: { id }});
+    async findOne(id: number): Promise<Task> {
+        return this.prisma.task.findUnique({ where: { id } });
     }
 
-    async updateTask(id: number, data: Prisma.TaskUpdateInput): Promise<Task>{
-        return this.prisma.task.update({
+    async updateTask(id: number, data: Prisma.TaskUpdateInput): Promise<Task> {
+        const updateTask = this.prisma.task.update({
             where: { id },
             data,
         });
+        const objectId = new Types.ObjectId(id);
+        const log = await this.taskModel.findByIdAndUpdate(objectId, data, { new: true }).exec()
+        await this.logService.createLog("Task Updated" + updateTask);
+
+        return updateTask
     }
 
-    async deleteTask(id: number): Promise<Task>{
-        return this.prisma.task.delete({
+    async deleteTask(id: number): Promise<Task> {
+        const deletedTask = this.prisma.task.delete({
             where: { id }
         })
+        const objectId = new Types.ObjectId(id);
+        const log = await this.taskModel.findByIdAndDelete(objectId).exec();
+        await this.logService.createLog("Task Deleted" + deletedTask)
+        return deletedTask
     }
 }
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { TasksController } from './task.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { LogsService } from 'src/logs/logs.service';
+import { Log, LogSchema } from "../logs/schemas/log.schema";
+import { LogsModule } from 'src/logs/logs.module';
 
 @Module({
+    imports: [
+        MongooseModule.forFeature([{ name: Log.name, schema: LogSchema }]),
+        LogsModule, 
+      ],
     providers: [TaskService, PrismaService],
     controllers: [TasksController],
 })
